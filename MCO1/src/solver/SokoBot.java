0fee2046fca6c0565ae949f35ff8eb2f62aca52a
Julia de Veyra <julianne_deveyra@dlsu.edu.ph>
2023-10-18 14:11:19 +0800

    partially working solution

--- MCO1/src/solver/SokoBot.java
+++ MCO1/src/solver/SokoBot.java
@@ -1,22 +1,104 @@
 package solver;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class SokoBot {
 
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    /*
-     * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
-     */
-    /*
-     * Default stupid behavior: Think (sleep) for 3 seconds, and then return a
-     * sequence
-     * that just moves left and right repeatedly.
-     */
+    
+    for (int h = 0; h < height; h++){
+      for (int w = 0; w < width; w++){
+        System.out.print(mapData[h][w]);
+      }
+      System.out.println();
+    }
+
+    setMapCoordinates(width, height, mapData);
+    setItemCoordinates(width, height, itemsData);
+
+    // A STAR IMPLEMENTATION
     try {
-      Thread.sleep(3000);
+      // sort states by heuristic + cost (length of moves)
+      StateComparator comparator = new StateComparator();
+      PriorityQueue<State> states = new PriorityQueue<State>(comparator);
+      State intial = new State(walls, crates, goals, player, "", width, height);
+      states.add(intial);
+
+      // keep track of visited states from the queue
+      ArrayList<State> visited = new ArrayList<State>();
+
+      // A STAR LOOP
+      // keep looping until queue is empty
+      while (true){
+        State currState = states.poll(); // get a state
+        visited.add(currState); // set as visited
+
+        if (!currState.checkGoal()){
+          currState.populateSuccessors();
+          for (int i = 0;  i < currState.getSuccessors().size(); i++){
+            if (!visited.contains(currState.getSuccessors().get(i))){
+              states.add(currState.getSuccessors().get(i)); // get curr state's successors
+            }
+          }
+        }
+        else {
+          return currState.getPath().toString();
+        }
+
+        for (int j = 0; j < visited.size(); j++){
+          System.out.println(visited.get(j));
+        }
+      }
+
+    } catch (NullPointerException e) {
+      System.out.println("UNSOLVABLE");
     } catch (Exception ex) {
       ex.printStackTrace();
     }
-    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
+    return "";
   }
+
+  private void setMapCoordinates(int width, int height, char[][] mapData){
+    walls = new ArrayList<Coordinate>();
+    goals = new ArrayList<Coordinate>();
+
+    for (int h = 0; h < height; h++){
+      for (int w = 0; w < width; w++){
+        // WALLS
+        if (mapData[h][w] == '#'){
+          Coordinate tempWall = new Coordinate(h, w);
+          this.walls.add(tempWall);
+        }
+        // GOALS
+        else if (mapData[h][w] == '.'){
+          Coordinate tempGoal = new Coordinate(h, w);
+          this.goals.add(tempGoal);
+        }
+      }
+    }
+  }
+
+  private void setItemCoordinates(int width, int height, char[][] itemsData){
+    crates = new ArrayList<Coordinate>();
+
+    for (int h = 0; h < height; h++){
+      for (int w = 0; w < width; w++){
+        // PLAYER
+        if (itemsData[h][w] == '@'){
+          this.player = new Coordinate(h, w);
+        }
+        // CRATES
+        else if (itemsData[h][w] == '$'){
+          this.crates.add(new Coordinate(h, w));
+        }
+      }
+    }
+  }
+
+  private ArrayList<Coordinate> walls;
+  private ArrayList<Coordinate> crates;
+  private ArrayList<Coordinate> goals;
+  private Coordinate player;
 
 }
--- MCO1/src/solver/State.java
+++ MCO1/src/solver/State.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class State {
 
     public State(ArrayList<Coordinate> walls, ArrayList<Coordinate> crates, ArrayList<Coordinate> goals,
-                Coordinate player, StringBuilder path, int width, int height){
+                Coordinate player, String path, int width, int height){
             this.walls = walls;
             this.crates = crates;
             this.goals = goals;
             this.player = player;
-            successors = new ArrayList<>();
+            successors = new ArrayList<State>();
             this.path = path;
             this.width = width;
             this.height = height;
@@ -18,7 +18,7 @@ public class State {
         }
     
     // computation for heuristics
-    private int manhattanDistance(){
+    private void manhattanDistance(){
         int x = player.getX();
         int y = player.getY();
         int manhattanA = 0; //player to crate
@@ -37,14 +37,15 @@ public class State {
                 manhattanB += Math.abs(tempCrate.getX() - tempGoal.getX()) + Math.abs(tempCrate.getY() - tempGoal.getY());
             }
         }
-        return manhattanA + manhattanB;
+        heuristic = manhattanA + manhattanB;
     }
 
     public int getHeuristic(){
+        manhattanDistance();
         return heuristic;
     }
 
-    public StringBuilder getPath(){
+    public String getPath(){
         return path;
     }
 
@@ -99,13 +100,14 @@ public class State {
         // make the move
         State next;
         if (movePlayer){
-            next = new State(walls, crates, goals, newPlayer, path.append(direction), width, height);
+
+            next = new State(walls, crates, goals, newPlayer, path + direction, width, height);
         }
         else if (moveCrate){
-            ArrayList<Coordinate> temp = new ArrayList<>(crates);
+            ArrayList<Coordinate> temp = new ArrayList<Coordinate>(crates);
             temp.remove(newPlayer);
             temp.add(newCrate);
-            next = new State(walls, temp, goals, newPlayer, path.append(direction), width, height);
+            next = new State(walls, temp, goals, newPlayer, path + direction, width, height);
         }
         else{
             next = null;
@@ -115,7 +117,56 @@ public class State {
         if (next != null){
             successors.add(next);
         }
+    }
 
+    public void populateSuccessors(){
+        int tempX = player.getX();
+        int tempY = player.getY();
+        move('u', new Coordinate(tempX-1, tempY), new Coordinate(tempX-2, tempY));
+        move('d', new Coordinate(tempX+1, tempY), new Coordinate(tempX+2, tempY));
+        move('l', new Coordinate(tempX, tempY-1), new Coordinate(tempX, tempY-2));
+        move('r', new Coordinate(tempX, tempY+1), new Coordinate(tempX, tempY+2));
+    }
+
+    public ArrayList<State> getSuccessors(){
+        return successors;
+    }
+
+    public boolean checkGoal(){
+        int n = 0;
+
+        for(int i = 0; i < crates.size(); i++){
+            if (goals.contains(crates.get(i))){
+                n++;
+            }
+        }
+        System.out.println(n);
+        System.out.println(path.toString());
+        if (n == goals.size()) return true;
+        return false;
+    }
+
+    public ArrayList<Coordinate> getCrates(){
+        return crates;
+    }
+
+    public Coordinate getPlayer(){
+        return player;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj instanceof State){
+            State temp = (State) obj;
+            ArrayList<Coordinate> tempCrate1 = crates;
+            ArrayList<Coordinate> tempCrate2 = temp.getCrates();
+            Coordinate tempPlayer = temp.getPlayer();
+            
+            if (tempCrate1.equals(tempCrate2) && player.equals(tempPlayer)){
+                return true;
+            }
+        }
+         return false;
     }
     
     private ArrayList<Coordinate> walls;
@@ -123,7 +174,7 @@ public class State {
     private ArrayList<Coordinate> goals;
     private Coordinate player;
     private ArrayList<State> successors;
-    private StringBuilder path;
+    private String path;
     private int heuristic;
     private int width;
     private int height;
