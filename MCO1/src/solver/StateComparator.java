15cbe5952b0e7a3f65cf5c4d58a212cc4c7d5b8a
Julia de Veyra <julianne_deveyra@dlsu.edu.ph>
2023-10-20 22:50:35 +0800

    simplified statecomparator

--- MCO1/src/solver/StateComparator.java
+++ MCO1/src/solver/StateComparator.java
@@ -5,9 +5,6 @@ import java.util.Comparator;
 public class StateComparator implements Comparator<State> {
     @Override
     public int compare(State s1, State s2){
-        int cost1 = s1.getPath().length();
-        int cost2 = s2.getPath().length();
-
-        return Integer.compare(cost1 + s1.getHeuristic(), cost2 + s2.getHeuristic());
+        return Integer.compare(s1.getPath().length() + s1.getHeuristic(), s2.getPath().length() + s2.getHeuristic());
     }
 }
