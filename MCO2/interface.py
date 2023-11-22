ca823a29d430170c4366ed0936fad876296ec2cd
Julia de Veyra <julianne_deveyra@dlsu.edu.ph>
2023-11-22 16:24:36 +0800

    updated dictionary

--- MCO2/interface.py
+++ MCO2/interface.py
@@ -4,13 +4,23 @@ prolog = Prolog()
 prolog.consult("logic.pl")
 
 statements = {
-    r'(\w+) and (\w+) are siblings\.$',
-    r'(\w+) is a sister of (\w+)\.$',
-    r'(\w+) is the mother of (\w+)\.$'
+    1: r'(\w+) and (\w+) are siblings\.$',
+    2: r'(\w+) is a sister of (\w+)\.$',
+    3: r'(\w+) is the mother of (\w+)\.$',
+    4: r'(\w+) is a grandmother of (\w+)\.$',
+    5: r'(\w+) is a child of (\w+)\.$',
+    6: r'(\w+) is a daughter of (\w+)\.$',
+    7: r'(\w+) is an uncle of (\w+)\.$',
+    8: r'(\w+) is a brother of (\w+)\.$',
+    9: r'(\w+) is the father of (\w+)\.$',
+    10: r'(\w+) and (\w+) are the parents of (\w+)\.$',
+    11: r'(\w+) is a grandfather of (\w+)\.$',
+    12: r'(\w+) is a son of (\w+)\.$',
+    13: r'(\w+) is an aunt of (\w+)\.$',
 }
 
 def parse_input(input):
-    for pattern in statements:
+    for key, pattern in statements.items():
         match = re.match(pattern, input)
         if match:
             groups = match.groups()
