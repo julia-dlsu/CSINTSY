746823faa37452f7b6e4c434cb802a839b3be877
Julia de Veyra <julianne_deveyra@dlsu.edu.ph>
2023-11-22 18:06:04 +0800

    updated parsing of statements to querying prolog

--- MCO2/interface.py
+++ MCO2/interface.py
@@ -13,10 +13,11 @@ statements = {
     7: r'(\w+) is an uncle of (\w+)\.$',
     8: r'(\w+) is a brother of (\w+)\.$',
     9: r'(\w+) is the father of (\w+)\.$',
-    10: r'(\w+) and (\w+) are the parents of (\w+)\.$',
-    11: r'(\w+) is a grandfather of (\w+)\.$',
-    12: r'(\w+) is a son of (\w+)\.$',
-    13: r'(\w+) is an aunt of (\w+)\.$',
+    10: r'(.+?) are children of (\w+)\.$',
+    11: r'(\w+) and (\w+) are the parents of (\w+)\.$',
+    12: r'(\w+) is a grandfather of (\w+)\.$',
+    13: r'(\w+) is a son of (\w+)\.$',
+    14: r'(\w+) is an aunt of (\w+)\.$',
 }
 
 def parse_statement(input):
@@ -24,16 +25,55 @@ def parse_statement(input):
         match = re.match(pattern, input)
         if match:
             groups = match.groups()
+            statement_key = key
             print(f"Statement: {groups}")
+            process_statement(key, groups)
             return
     print("Input not recognized.")
 
+def process_statement(key, names):
+    if key == 1:
+        soln = prolog.query(f"add_sibling({names[0]}, {names[1]})")
+    elif key == 2:
+        soln = prolog.query(f"add_sister({names[0]}, {names[1]})")
+    elif key == 3:
+        soln = prolog.query(f"add_mother({names[0]}, {names[1]})")
+    elif key == 4:
+        soln = prolog.query(f"add_grandmother({names[0]}, {names[1]})")
+    elif key == 5:
+        soln = prolog.query(f"add_child({names[0]}, {names[1]})")
+    elif key == 6:
+        soln = prolog.query(f"add_daughter({names[0]}, {names[1]})")
+    elif key == 7:
+        soln = prolog.query(f"add_uncle({names[0]}, {names[1]})")
+    elif key == 8:
+        soln = prolog.query(f"add_brother({names[0]}, {names[1]})")
+    elif key == 9:
+        soln = prolog.query(f"add_father({names[0]}, {names[1]})")
+    elif key == 10:
+        children = re.findall(r'\b(?!and\b)\w+', names[0])
+        for x in children:
+            soln = prolog.query(f'add_child({x}, {names[1]})')
+    elif key == 11:
+        soln = prolog.query(f"add_parent({names[0]}, {names[2]})")
+        soln = prolog.query(f"add_parent({names[1]}, {names[2]})")
+    elif key == 12:
+        soln = prolog.query(f"add_grandfather({names[0]}, {names[1]})")
+    elif key == 13:
+        soln = prolog.query(f"add_son({names[0]}, {names[1]})")
+    elif key == 14:
+        soln = prolog.query(f"add_aunt({names[0]}, {names[1]})")
+
+    if check_query(soln):
+        print("OK! I learned something.")
+    else:
+        print("That's impossible!")
+
 def check_query(x):
     if list(x):
-        q_result = True
+        return True
     else:
-        q_result = False
-    print(q_result, "\n")
+        return False
 
 chat_input = input("Enter a statement: ")
 parse_statement(chat_input)
