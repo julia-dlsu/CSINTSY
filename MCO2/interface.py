76d5f890fcc3d4f0a13ef9119ff5093e1a3f01db
Julia de Veyra <julianne_deveyra@dlsu.edu.ph>
2023-11-23 23:34:55 +0800

    fixed logic and interface

--- MCO2/interface.py
+++ MCO2/interface.py
@@ -1,7 +1,9 @@
 import re
+import os
 from pyswip import Prolog
 prolog = Prolog()
 prolog.consult("logic.pl")
+os.system('cls')
 
 statements = {
     1: r'(\w+) and (\w+) are siblings\.$',
@@ -20,16 +22,120 @@ statements = {
     14: r'(\w+) is an aunt of (\w+)\.$',
 }
 
+#format
+#key: [pattern, type]
+#qtype =
+#0: true or false question || 1: enumeration
+questions = {
+    1: [r'are (\w+) and (\w+) siblings\?$', 0],
+    2: [r'is (\w+) a sister of (\w+)\?$', 0] ,
+    3: [r'is (\w+) a brother of (\w+)\?$', 0],
+    4: [r'is (\w+) the mother of (\w+)\?$', 0],
+    5: [r'is (\w+) the father of (\w+)\?$', 0],
+    6: [r'are (\w+) and (\w+) the parents of (\w+)\?$', 0],
+    7: [r'is (\w+) a grandmother of (\w+)\?$', 0],
+    8: [r'is (\w+) a daughter of (\w+)\?$', 0],
+    9: [r'is (\w+) a son of (\w+)\?$', 0],
+    10: [r'is (\w+) a child of (\w+)\?$', 0],
+    11: [r'are (.+?) children of (\w+)\?$', 0],
+    12: [r'is (\w+) an uncle of (\w+)\?$', 0],
+    13: [r'who are the siblings of (\w+)\?$', 1],
+    14: [r'who are the sisters of (\w+)\?$', 1],
+    15: [r'who are the brothers of (\w+)\?$', 1],
+    16: [r'who is the mother of (\w+)\?$', 1],
+    17: [r'who is the father of (\w+)\?$', 1],
+    18: [r'who are the parents of (\w+)\?$', 1],
+    19: [r'is (\w+) a grandfather of (\w+)\?$', 0],
+    20: [r'who are the daughters of (\w+)\?$', 1],
+    21: [r'who are the sons of (\w+)\?$', 1],
+    22: [r'who are the children of (\w+)\?$', 1],
+    23: [r'is (\w+) an aunt of (\w+)\?$', 0],
+    24: [r'are (\w+) and (\w+) relatives\?$', 0]
+}
+
 def parse_statement(input):
     for key, pattern in statements.items():
-        match = re.match(pattern, input)
+        match = re.match(pattern, input, flags=re.IGNORECASE)
         if match:
             groups = match.groups()
-            statement_key = key
-            print(f"Statement: {groups}")
             process_statement(key, groups)
-            return
-    print("Input not recognized.")
+            return True
+    return False
+
+def parse_question(input):
+    for key, val in questions.items():
+        match = re.match(val[0], input, flags=re.IGNORECASE)
+        if match:
+            groups = match.groups()
+            process_question(key, groups, val[1])
+            return True
+    return False
+
+def process_question(key, names, qtype):
+    if key == 1:
+        soln = prolog.query(f"sibling_of({names[0]}, {names[1]})")
+    elif key == 2:
+        soln = prolog.query(f"sister_of({names[0]}, {names[1]})")
+    elif key == 3:
+        soln = prolog.query(f"brother_of({names[0]}, {names[1]})")
+    elif key == 4:
+        soln = prolog.query(f"mother_of({names[0]}, {names[1]})")
+    elif key == 5:
+        soln = prolog.query(f"father_of({names[0]}, {names[1]})")
+    elif key == 6:
+        soln0 = prolog.query(f"parent_of({names[0]}, {names[2]})")
+        soln1 = prolog.query(f"parent_of({names[1]}, {names[2]})")
+    elif key == 7:
+        soln = prolog.query(f"grandmother_of({names[0]}, {names[1]})")
+    elif key == 8:
+        soln = prolog.query(f"daughter_of({names[0]}, {names[1]})")
+    elif key == 9:
+        soln = prolog.query(f"son_of({names[0]}, {names[1]})")
+    elif key == 10:
+        soln = prolog.query(f"child_of({names[0]}, {names[1]})")
+    elif key == 11:
+        children = re.findall(r'\b(?!and\b)\w+', names[0])
+        count = 0
+        for x in children:
+            soln = prolog.query(f"child_of({x}, {names[1]})")
+            soln = list(soln)
+            if check_query(soln): count += 1
+        if count == len(children): print("Yes.")
+        else: print("No.")
+    elif key == 12:
+        soln = prolog.query(f"uncle_of({names[0]}, {names[1]})")
+    elif key == 13:
+        query = f"sibling_of(X, {names[0]})"
+    elif key == 14:
+        query = f"sister_of(X, {names[0]})"
+    elif key == 15:
+        query = f"brother_of(X, {names[0]})"
+    elif key == 16:
+        query = f"mother_of(X, {names[0]})"
+    elif key == 17:
+        query = f"father_of(X, {names[0]})"
+    elif key == 18:
+        query = f"parent_of(X, {names[0]})"
+    elif key == 19:
+        soln = prolog.query(f"grandfather_of({names[0]}, {names[1]})")
+    elif key == 20:
+        query = f"daugher_of(X, {names[0]})"
+    elif key == 21:
+        query = f"son_of(X, {names[0]})"
+    elif key == 22:
+        query = f"child_of(X, {names[0]})"
+    elif key == 23:
+        soln = prolog.query(f"aunt_of({names[0]}, {names[1]})")
+    elif key == 24:
+        soln = prolog.query(f"relative_of({names[0]}, {names[1]})")
+        
+    if qtype == 0 and key != 11:
+        if check_query(soln):
+            print("Yes!")
+        else:
+            print("No!")
+    elif qtype == 1:
+        enumerate_query(query)
 
 def process_statement(key, names):
     if key == 1:
@@ -52,11 +158,28 @@ def process_statement(key, names):
         soln = prolog.query(f"add_father({names[0]}, {names[1]})")
     elif key == 10:
         children = re.findall(r'\b(?!and\b)\w+', names[0])
+        wrong = []
+        count = 0
         for x in children:
             soln = prolog.query(f'add_child({x}, {names[1]})')
+            soln = list(soln)
+            if check_query(soln): count += 1
+            else: wrong.append(x)
+        if count == len(children): print("OK! I learned something.")
+        elif count > 0 and count < len(children): print(" and ".join(wrong), " cannot be added as children.")
+        else: print("That's impossible!")
     elif key == 11:
-        soln = prolog.query(f"add_parent({names[0]}, {names[2]})")
-        soln = prolog.query(f"add_parent({names[1]}, {names[2]})")
+        parents = [names[0], names[1]]
+        wrong = []
+        count = 0
+        for x in parents:
+            soln = prolog.query(f"add_parent({x}, {names[2]})")
+            soln = list(soln)
+            if check_query(soln): count += 1
+            else: wrong.append(x)
+        if count == len(parents): print("OK! I learned something.")
+        elif count > 0 and count < len(parents): print(f"{wrong[0]} cannot be added as a parent.")
+        else: print("That's impossible!")
     elif key == 12:
         soln = prolog.query(f"add_grandfather({names[0]}, {names[1]})")
     elif key == 13:
@@ -64,16 +187,39 @@ def process_statement(key, names):
     elif key == 14:
         soln = prolog.query(f"add_aunt({names[0]}, {names[1]})")
 
-    if check_query(soln):
+    if check_query(soln) and key != 10 and key != 11:
         print("OK! I learned something.")
-    else:
+    elif not(check_query(soln)) and key != 10:
         print("That's impossible!")
 
 def check_query(x):
-    if list(x):
-        return True
-    else:
-        return False
+    if list(x): return True
+    return False
 
-chat_input = input("Enter a statement: ")
-parse_statement(chat_input)
+def enumerate_query(query):
+    soln = prolog.query(query)
+    prev = []
+    if check_query(soln):
+        for soln in prolog.query(query):
+            if soln["X"] not in prev:
+                print(soln["X"].capitalize())
+            prev.append(soln["X"])
+    else:
+        print("There is no valid person for that category.")
+
+def take_input():
+    while(True):
+        chat_input = input("> ")
+        if chat_input == "exit":
+            exit()
+        else:
+            check_statement = parse_statement(chat_input.lower())
+            if not check_statement:
+                check_question = parse_question(chat_input.lower())
+            if not check_statement and not check_question:
+                print("[Error] Invalid input.")
+
+if __name__ == "__main__":
+    print("[Expert System Prototype: Family Tree Chatbot]")
+    print("Authors: Choa, de Veyra, Fortiz\n")
+    take_input()
--- MCO2/logic.pl
+++ MCO2/logic.pl
@@ -26,12 +26,19 @@
 :- dynamic uncle_of/2.
 :- dynamic add_father/2.
 :- dynamic add_mother/2.
+:- dynamic brother/2.
+:- dynamic sister/2.
+:- dynamic brother_of/2.
+:- dynamic sister_of/2.
+
 
 % A is either a male or female but not both
 not(is_a(A, male)) :- is_a(A,female).
 not(is_a(A, female)) :- is_a(A,male).
 
-is_relative(A,B) :-
+
+% A is a relative of B if role has connection to it
+relative_of(A,B) :-
     %scenario: sibling of your parents
     %scenario: children of the sibling of your parents
     (parent_of(A,B), !);
@@ -40,95 +47,126 @@ is_relative(A,B) :-
     (grandparent_of(B,A), !);
     (child_of(A,B), !);
     (child_of(B,A), !);
-    (sibling_of(A,B), !).
+    (sibling_of(A,B), !);
+    (aunt_of(A,B),!);
+    (aunt_of(B,A),!);
+    (uncle_of(A,B),!);
+    (uncle_of(B,A),!);
+    %cousin check
+    (parent_of(C,A), sibling_of(C,D), child_of(B,D));
+    (parent_of(C,B), sibling_of(C,D), child_of(A,D)).
+
 
 %A is the child of B if they are a child or he/she is a son or daughter of B.
 child_of(A,B) :-
-    (child(A,B); son(A,B); daughter(A,B), A\==B).
+    ((child(A,B)); son(A,B); daughter(A,B), A\==B).
+
 
 %A is the parent of B if they are a child or he/she is a son or daughter of A.
 parent_of(A,B) :-
-    ((child(B,A); son(B,A); daughter(B,A), A\==B),!);
-    parent(A,B), A\==B.
+    (parent(A,B));
+    ((child(B,A); son(B,A); daughter(B,A), A\==B)).
+    %(sibling(B,C), (parent(D,C), parent(E,C), D\==E), parent(X,B), (X==D; X==E), (parent(A,B); assertz(parent(A,B))), !).
+    %(sibling(B,C), parent(D,C), parent(E,C), D\==E, parent(X,B), \+ parent(Y,B), X\==Y, (X\==D; X\==E), (parent(A,B); assertz(parent(A,B))), !).
 
 mother_of(A,B):-
     A\==B,
     \+ is_a(A,male),
-    child_of(B,A);
-    (sibling(C,B), father(D,C), father(E,B), mother(A,C)).
+    mother(A,B);
+    child(B,A);
+    (sibling(C,B), father(D,C), father(E,B), mother(A,C));
+    ((sibling(B,C), father(D,C), father(E,B), mother(A,C)), (mother(A,B);assertz(mother(A,B))));
+    ((sibling(B,C), father(D,C), father(E,B)), (assertz(mother(A,B)), assertz(mother(A,C)))). %Untested
 
 father_of(A,B):-
     A\==B,
     \+ is_a(A,female),
-    child_of(B,A);
-    (sibling(C,B), mother(D,C), mother(E,B), father(A,C)).
+    father(A,B);
+    child(B,A);
+    (sibling(C,B), mother(D,C), mother(E,B), father(A,C));
+    ((sibling(B,C), mother(D,C), mother(E,B), father(A,C)), (father(A,B);assertz(father(A,B))));
+    ((sibling(B,C), mother(D,C), mother(E,B)), (assertz(father(A,B)), assertz(father(A,C)))). %Untested
 
-
-grandparent_of(A,B) :-
-    grandparent(A,B);
-    (parent(C,B), parent(A,C));  %fix multiple grandparents
-    (uncle(C,B),parent(A,C));
-    (aunt(C,B),parent(A,C)).
 
 % A is a sibling of B if (father/mother of A) and (father/mother of C)
 % is the same
 sibling_of(A,B) :-
     (parent(C,B), parent(C,A), A\==B);
-     sibling(A,B);
-     sibling(B,A);
+    (sibling(A,B));
+    (sibling(B,A));
     (parent(C,A), grandparent(C,D), child(D,B));
     (parent(C,B), grandparent(C,D), child(D,A));
     (uncle(A,C), parent(B,C));
     (uncle(B,C), parent(A,C));
     (aunt(A,C), parent(B,C));
-    (aunt(B,C), parent(A,C)).
+    (aunt(B,C), parent(A,C));
+    %(sibling(A,C), sibling(B,A));
+    ((sibling(B,C),sibling(A,C), A\==B, B\==C, C\==A), (sibling(B,A); assertz(sibling(B,A)))).
 
 
 % A is a brother/sister of B
+brother_of(A,B) :-
+    is_a(A,male), sibling(A,B);
+    brother(A,B);
+    ((sibling(A,C), sibling(C,B), is_a(A,male)), (sibling(A,B); assertz(sibling(A,B))));
+    (parent(C,A),parent(C,B), is_a(A,male)), (brother(A,B); assertz(brother(A,B))),(sibling(A,B); assertz(sibling(A,B))).
 
-brother_of(A,B) :-  is_a(A,male), sibling_of(A,B).
-sister_of(A,B) :- is_a(A,female), sibling_of(A,B).
+sister_of(A,B) :-
+    is_a(A,female), sibling(A,B);
+    sister(A,B);
+    ((sibling(A,C), sibling(C,B), is_a(A,female)), (sibling(A,B); assertz(sibling(A,B))));
+    (parent(C,A),parent(C,B), is_a(A,female)), (sister(A,B); assertz(sister(A,B))),(sibling(A,B); assertz(sibling(A,B))).
 
 
 % A is a daughter/son of B
+daughter_of(A,B) :-
+    is_a(A,female), child(A,B).
 
-daughter_of(A,B) :- is_a(A,female), child(A,B).
-son_of(A,B) :- is_a(A,male), child(A,B).
+son_of(A,B) :-
+    is_a(A,male), child(A,B).
+
 
 % A is the grandfather/grandmother of B
+grandparent_of(A,B) :-
+    (grandparent(A,B));
+    (parent(C,B), parent(A,C));  %fix multiple grandparents
+    (parent(A,C), uncle(C,B));
+    (parent(A,C),aunt(C,B)).
+
 grandfather_of(A,B) :-
     \+ is_a(A,female),
     grandfather(A,B);
-    (parent(C,B), parent(A,C));  %fix multiple grandparents
-    (uncle(C,B),parent(A,C));
-    (aunt(C,B),parent(A,C)).
+    ((parent(C,B), parent(A,C)), (grandfather(A,B); assertz(grandfather(A,B)), assertz(grandparent(A,B))),!);  %fix multiple grandparents
+    ((uncle(C,B),parent(A,C)), (grandfather(A,B); assertz(grandfather(A,B)), assertz(grandparent(A,B))),!);
+    ((aunt(C,B),parent(A,C)), (grandfather(A,B); assertz(grandfather(A,B)), assertz(grandparent(A,B))),!).
 
 grandmother_of(A,B) :-
     \+ is_a(A,male),
     grandmother(A,B);
-    (parent(C,B), parent(A,C));  %fix multiple grandparents
-    (uncle(C,B),parent(A,C));
-    (aunt(C,B),parent(A,C)).
+    ((parent(C,B), parent(A,C)), (grandmother(A,B); assertz(grandmother(A,B)), assertz(grandparent(A,B))),!);
+    ((uncle(C,B),parent(A,C)), (grandmother(A,B); assertz(grandmother(A,B)), assertz(grandparent(A,B))),!);
+    ((aunt(C,B),parent(A,C)), (grandmother(A,B); assertz(grandmother(A,B)), assertz(grandparent(A,B))),!).
+
 
 % A is an aunt/uncle of B
 aunt_of(A,B) :-
      A\==B,
      \+ is_a(A,male),
-     aunt(A,B);
+     (aunt(A,X), X==B);
      (sibling(A,C), child(B,C));
      (sibling(A,C), uncle(C,B));
      (parent(C,A), grandparent(C,B)).
 
-
 uncle_of(A,B) :-
     A\==B,
     \+ is_a(A,female),
-    uncle(A,B);
-    (sibling_of(A,C), child_of(B,C));
+    (uncle(A,X), X==B);
+    (sibling(A,C), child(B,C));
     (sibling(A,C), aunt(C,B));
     (parent(C,A), grandparent(C,B)).
 
-%A is the child of B
+
+%======== INSERTING FACTS + INFER ==========
 add_child(A,B) :-
     A\==B,
     \+ parent_of(A,B), \+ parent_of(B,A),
@@ -143,9 +181,6 @@ add_child(A,B) :-
     (parent(B,A) ; assertz(parent(B,A))),
     (child(A,B) ; assertz(child(A,B))),
     ((is_a(A, male), assertz(son(A,B)),!) ; (is_a(A, female), assertz(daughter(A,B)),!) ; true).
-
-
-%A is the son/daughter of B
 
 add_son(A,B) :-
     A\==B,
@@ -177,49 +212,67 @@ add_daughter(A,B) :-
     (child(A,B) ; assertz(child(A,B))),
     (is_a(A,female) ; assertz(is_a(A,female)), !).
 
-%A is the parent of B
-
-add_parent(A,B):-
-     A\==B,
-    \+ parent_of(B,A),
-    \+ (father(_,B), mother(_,B)),
-    \+ sibling_of(A,B),
-    \+ grandparent_of(A,B), \+grandparent_of(B,A),
-    \+ (grandparent_of(C,B), parent_of(A,C)),
-    \+ (child_of(C,A), grandparent_of(B,C)),
-
-    (((sibling(B,C), parent(Y,C), parent(X,C), parent(Z,B), Y\==X, Y\==Z, X\==Z, A==Y; A==X),
-    (parent(A,B); assertz(parent(A, B))),!) ;
-
-    (sibling(_,C); parent(A,B) ; (assertz(parent(A,B)),!))),
-
-    (child(B,A) ; (assertz(child(B,A)),!)),
-    ((is_a(B, male), assertz(son(B, A))) ; (is_a(B, female), assertz(daughter(B, A))) ; true).
-
-
-%A is the mother/father of B
+add_parent(A, B):-
+    A \== B,
+    \+ parent_of(B, A),
+    \+ (parent(X,B), parent(Y,B), X\==Y),
+    \+ (father(_, B), mother(_, B)),
+    \+ sibling_of(A, B),
+    (
+           (sibling(B,C), (parent(X,C), parent(Y,C), parent(Z,B), X\==Y, Z\==X, Z\==Y, (A==X;A==Y))); %B has a sibling C -> C has parents X and Y -> B has parent Z
+           (sibling(B,C), (parent(X,C), parent(Y,C), \+parent(_,B), X\==Y, Z\==X)); %B has a sibling C -> C has parents X and Y -> B has no parents
+           (sibling(B,C), (parent(X,C), \+parent(Y,C), \+parent(_,B)), X\==Y); %B has a sibling C -> C has a parent X -> B has no parents
+           (sibling(B,C), (\+parent(_,C), parent(_,B))); % B has a sibling C -> C has no parents -> B has a parent
+           (sibling(B,C), (\+parent(_,C), \+parent(_,B))); %B has a sibling C -> C has no parents -> B has no parents
+           \+sibling(_,B) %B has no siblings
+    ),
+    \+ grandparent_of(A, B),
+    \+ grandparent_of(B, A),
+    \+ (grandparent_of(C, B), parent_of(A, C)),
+    \+ (child_of(C, A), grandparent_of(B, C)),
+    %(sibling(B,C), parent(D,C), parent(E,C), parent(X,B), (X\==D, X\==E), (A == D; A == E)),
+    %((sibling(B, C), parent(Y, C), parent(X, C), parent(Z, B), Y \== X, Y \== Z, X \== Z, (A == Y; A == X)),
+    %  (parent(A, B), \+ (child(B, _), child(B, _))), !);
+    (\+ parent(A, B) , (assertz(parent(A, B)), !)), %MODIFIED: so that add_parent returns false if A is already a parent of B
+    (child(B, A) ; (assertz(child(B, A)), !)),
+    (
+        (is_a(B, male), assertz(son(B, A))) ;
+        (is_a(B, female), assertz(daughter(B, A))) ;
+        true
+    ).
 
 add_father(A,B) :-
     A\==B,
     \+ is_a(A,female),
     \+ father(_,B),
+    (
+           (\+parent(_,B)); %B has no parents
+           (parent(X,B), parent(Y,B), X\==Y, (A==X; A==Y;false)); %B has two parents
+           (parent(X,B), \+ (parent(Y,B), X\==Y)) %B has one parent
+    ),
     \+ parent_of(B,A),
     \+ sibling_of(A,B),
-    \+ grandparent_of(B,A),\+ grandparent_of(A,B),
+    \+ uncle_of(A,B),
+    \+ grandparent_of(B,A),
+    \+ grandparent_of(A,B),
     \+ (grandparent_of(C,B), parent_of(A,C)),
     \+ (child_of(C,A), grandparent_of(B,C)),
     (father(A,B); assertz(father(A,B))),
     (parent(A,B) ; assertz(parent(A,B))),
     (child(B, A) ; assertz(child(B,A))),
     (is_a(A, male); assertz(is_a(A,male))),
-    ((is_a(B, male), assertz(son(B, A))) ; (is_a(B, female), assertz(daughter(B, A))) ; true);
-    ((sibling(B,C), mother(D,C), mother(E,B), father(A,C)), assertz(father(A,B)));
-    ((sibling(B,C), mother(D,C), mother(E,B)), (assertz(father(A,B), assertz(father(A,C))))).
+    ((is_a(B, male), assertz(son(B, A))) ; (is_a(B, female), assertz(daughter(B, A))) ; true).
 
 add_mother(A,B) :-
     A\==B,
     \+ is_a(A,male),
     \+ mother(_,B),
+    (
+           (\+parent(_,B)); %B has no parents
+           (parent(X,B), parent(Y,B), X\==Y, (A==X; A==Y; false)); %B has two parents
+           (parent(X,B), \+ (parent(Y,B), X\==Y)) %B has one parent
+           %(\+parent(Q,B), \+parent(P,B), Q\==P)
+    ),
     \+ parent_of(B,A),
     \+ sibling_of(A,B),
     \+ aunt_of(A,B),
@@ -231,11 +284,8 @@ add_mother(A,B) :-
     (parent(A,B) ; assertz(parent(A,B))),
     (child(B,A) ; assertz(child(B,A))),
     (is_a(A,female); assertz(is_a(A,female))),
-    ((is_a(B, male), assertz(son(B, A))) ; (is_a(B, female), assertz(daughter(B, A))) ; true);
-    ((sibling(B,C), father(D,C), father(E,B), mother(A,C)), assertz(mother(A,B)));
-    ((sibling(B,C), father(D,C), father(E,B)), (assertz(mother(A,B), assertz(mother(A,C))))).
+    ((is_a(B, male), assertz(son(B, A))) ; (is_a(B, female), assertz(daughter(B, A))) ; true).
 
-%A is the grandfather/grandmother of B
 add_grandfather(A,B) :-
     A\==B,
     \+ is_a(A, female),
@@ -246,6 +296,10 @@ add_grandfather(A,B) :-
     \+ child_of(B,A),
     \+ child_of(A,B),
     \+ sibling_of(A,B),
+    \+ uncle_of(A,B),
+    \+ uncle_of(B,A),
+    \+ aunt_of(A,B),
+    \+ aunt_of(B,A),
     \+ (grandparent_of(C,A), parent_of(C,B)), %gender neutral uncle/aunt
     \+ (parent_of(C,A), grandparent_of(C,B)), %checking if A is an uncle/aunt of B
     (is_a(A, male); assertz(is_a(A,male))),
@@ -262,13 +316,16 @@ add_grandmother(A,B) :-
     \+ child_of(B,A),
     \+ child_of(A,B),
     \+ sibling_of(A,B),
+    \+ uncle_of(A,B),
+    \+ uncle_of(B,A),
+    \+ aunt_of(A,B),
+    \+ aunt_of(B,A),
     \+ (grandparent_of(C,A), parent_of(C,B)), %gender neutral uncle/aunt
     \+ (parent_of(C,A), grandparent_of(C,B)), %checking if A is an uncle/aunt of B
     (is_a(A, female); assertz(is_a(A,female))),
     (grandmother(A,B); assertz(grandmother(A,B))),
     (grandparent(A,B); assertz(grandparent(A,B))).
 
-%A is the aunt/uncle of B
 add_aunt(A,B) :-
     A\==B,
     \+ is_a(A,male),
@@ -295,15 +352,11 @@ add_uncle(A,B) :-
     \+ child_of(A,B),
     \+ sibling_of(A,B),
     (uncle(A,B); assertz(uncle(A,B))),
-    (is_a(A,male); assertz(is_a(A,male))),
-    (brother_of(A,C), child_of(D,C), sibling_of(B,D); assertz(uncle(A,D))), %Inheritance to sibling
-    (uncle(A,B); assertz(uncle(A,B))).
+    (is_a(A,male); assertz(is_a(A,male))).
+    %(brother_of(A,C), child_of(D,C), sibling_of(B,D); assertz(uncle(A,D))). %Inheritance to sibling
 
-%A is the sibling of B (gender can be obscure)
 add_sibling(A,B) :-
     A\==B,
-    (parent(C,A), parent(C,B), assertz(sibling(A,B)));
-    (parent(C,A), grandparent(C,B));
     \+ parent_of(B,A),
     \+ parent_of(A,B),
     \+ grandparent_of(A,B),
@@ -314,7 +367,48 @@ add_sibling(A,B) :-
     \+ aunt_of(B,A),
     \+ uncle_of(A,B),
     \+ uncle_of(B,A),
-    (sibling(A,B); assertz(sibling(A,B))).
+    (brother(A,B), (sibling(A,B); assertz(sibling(A,B))));
+    (sister(A,B), (sibling(A,B); assertz(sibling(A,B))));
+    ((parent(C,A), parent(C,B)), (sibling(A,B); assertz(sibling(A,B))));
+    ((parent(C,A), grandparent(C,B)), (parent(A,B); assertz(parent(A,B))));
+    (sibling(A,B); assertz(sibling(A,B)),(sibling(B,A); assertz(sibling(B,A)))).
+
+add_brother(A,B):-
+    A\==B,
+    \+ is_a(A,female),
+    \+ parent_of(B,A),
+    \+ parent_of(A,B),
+    \+ grandparent_of(A,B),
+    \+ grandparent_of(B,A),
+    \+ child_of(B,A),
+    \+ child_of(A,B),
+    \+ aunt_of(A,B),
+    \+ aunt_of(B,A),
+    \+ uncle_of(A,B),
+    \+ uncle_of(B,A),
+    ((sibling(A,B), is_a(A,male)), (brother(A,B), assertz(brother(A,B)),assertz(sibling(A,B)),  assertz(sibling(B,A))));
+    ((parent(C,A), parent(C,B)), (brother(A,B); (assertz(brother(A,B)), assertz(is_a(A,male)), assertz(sibling(A,B)),  assertz(sibling(B,A)))));
+    ((parent(C,A), grandparent(C,B)), (parent(A,B); assertz(parent(A,B))));
+    (brother(A,B); (assertz(brother(A,B)), assertz(is_a(A,male)), assertz(sibling(A,B)),  assertz(sibling(B,A)))).
+
+add_sister(A,B):-
+    A\==B,
+    \+ is_a(A,male),
+    \+ parent_of(B,A),
+    \+ parent_of(A,B),
+    \+ grandparent_of(A,B),
+    \+ grandparent_of(B,A),
+    \+ child_of(B,A),
+    \+ child_of(A,B),
+    \+ aunt_of(A,B),
+    \+ aunt_of(B,A),
+    \+ uncle_of(A,B),
+    \+ uncle_of(B,A),
+    ((sibling(A,B), is_a(A,female)), (sister(A,B), assertz(sister(A,B)), assertz(sibling(A,B)),  assertz(sibling(B,A))));
+    ((parent(C,A), parent(C,B)), (sister(A,B); (assertz(sister(A,B)), assertz(is_a(A,female)), assertz(sibling(A,B)),  assertz(sibling(B,A)))));
+    ((parent(C,A), grandparent(C,B)), (parent(A,B); assertz(parent(A,B))));
+    (sister(A,B); (assertz(sister(A,B)), assertz(is_a(A,female)), assertz(sibling(A,B)),  assertz(sibling(B,A)))).
+
 
 
 
